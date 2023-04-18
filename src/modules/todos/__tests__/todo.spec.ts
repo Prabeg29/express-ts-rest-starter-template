baed4c65920af993026eabb1660788490ae542e1
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-04-18 09:02:26 +0545

    Add test cases to query single todo
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- src/modules/todos/__tests__/todo.spec.ts
+++ src/modules/todos/__tests__/todo.spec.ts
@@ -67,15 +67,37 @@ describe('Todo APIs', () => {
   });
 
   describe('GET: /api/todos/:id', () => {
-    it('should return todo when id is valid', async () => {
-      const validId = '5454';
+    const id = 5454;
 
+    it('should return error when id is invalid', async () => {
       response = await request(server)
-        .get(`/api/todos/${validId}`)
+        .get(`/api/todos/${id}`)
         .set('Accept', 'application/json');
   
+      expect(response.status).toEqual(StatusCodes.NOT_FOUND);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Todo not found');
+    });
+
+    it('should return todo when id is valid', async () => {
+      await knexInstance.seed.run();
+
+      response = await request(server)
+        .get(`/api/todos/${id}`)
+        .set('Accept', 'application/json');
+        
       expect(response.status).toEqual(StatusCodes.OK);
       expect(response.body).toHaveProperty('todo');
+      expect(response.body.todo.id).toEqual(id);
+      expect(response.body.todo.attributes).toEqual({
+        title      : expect.any(String),
+        description: expect.any(String),
+        isComplete : expect.any(Number),
+        dueDate    : expect.any(String),
+        createdAt  : expect.any(String),
+        updatedAt  : expect.any(String)
+      });
+      expect(response.body.todo.meta.link).toEqual(`http://localhost:8848/api/todos/${id}`);
     });
   });
 });
