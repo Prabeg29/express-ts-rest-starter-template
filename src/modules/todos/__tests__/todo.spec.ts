c8895783aa20cd3d53a63ab62bfc6bcb30fa593f
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-04-20 08:08:16 +0545

    Add test for deleting a resource
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- src/modules/todos/__tests__/todo.spec.ts
+++ src/modules/todos/__tests__/todo.spec.ts
@@ -211,4 +211,30 @@ describe('Todo APIs', () => {
       });
     });
   });
+
+  describe('DELETE: /api/todos/:id', () => {
+    const id = 5454;
+
+    it('should return error when id is invalid', async () => {
+      response = await request(server)
+        .delete(`/api/todos/${id}`)
+        .set('Accept', 'application/json');
+  
+      expect(response.status).toEqual(StatusCodes.NOT_FOUND);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Todo not found');
+    });
+
+    it.only('should delete todo resource', async () => {
+      await knexInstance.seed.run();
+
+      response = await request(server)
+        .delete(`/api/todos/${id}`)
+        .set('Accept', 'application/json');
+  
+      expect(response.status).toEqual(StatusCodes.OK);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Todo deleted');
+    });
+  });
 });
